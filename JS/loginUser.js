eff6f8e11a38b0990cd1e0f97ae55fbfee10fed5
anewardeberg <anelan@hotmail.no>
2020-05-18 10:28:39 +0200

    modified loginUser

--- JS/loginUser.js
+++ JS/loginUser.js
@@ -2,10 +2,10 @@ function loginUser(event) {
     const username = document.querySelector("[name = 'usernameLogin']").value;
     const password = document.querySelector("[name = 'passwordLogin']").value;
 
-    const userInfo = JSON.parse(window.localStorage.getItem("users")) || [];
+    const user = JSON.parse(window.localStorage.getItem("users")) || [];
 
     for(const user of users){
-        if(username === userInfo.name && password === userInfo.password) {
+        if(username === user.username && password === user.password) {
             alert("login succeeded");
         }else {
             alert("incorrect username or password, please try again.");
