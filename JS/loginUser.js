a00adf9342f152a25f01ed9bdb8889051ccf1e7b
Benjamin <vedlogb@gmail.com>
2020-05-18 11:21:53 +0200

    fixed login

--- JS/loginUser.js
+++ JS/loginUser.js
@@ -2,13 +2,18 @@ function loginUser(event) {
     const username = document.querySelector("[name = 'usernameLogin']").value;
     const password = document.querySelector("[name = 'passwordLogin']").value;
 
-    const user = JSON.parse(window.localStorage.getItem("users")) || [];
+    const users = JSON.parse(window.localStorage.getItem("users"));
 
     for(const user of users){
-        if(username === user.username && password === user.password) {
-            alert("login succeeded");
+        if(username === user.username) {
+            if(password === user.password){
+                alert("login succeeded");
+            }else{
+                alert("incorrect username or password, please try again.");
+            }
+
         }else {
-            alert("incorrect username or password, please try again.");
+            console.log("couldnt find username. ");
         }
     }
 
