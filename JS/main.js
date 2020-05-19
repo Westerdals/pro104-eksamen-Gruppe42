30bb86016df74a76bb8b05f30202316ad04566f4
anewardeberg <anelan@hotmail.no>
2020-05-19 15:08:43 +0200

    filled an array with quotes

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,14 @@
 var quoteArray = [
     "To plant a garden is to believe in tomorrow. - Audrey Hepburn",
     "Watching something grow is good for morale. It helps us believe in life. - Myron S. Kaufman",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+    "Don't judge each day by the harvest you reap, but the seeds that you plant. - Robert Lewis Stevenson",
+    "We don't grow when things are easy.",
+    "The greatest threat to our planet is the belief that someone else will save it. - Robert Swan",
+    "Faith plants the seed. Love makes it grow.",
+    "Plant your own garden and decorate your own soul instead of waiting for someone to bring you flowers.",
+    "The best time to plant a tree was 20 years ago. The second best time is now.",
+    "Keep going, keep growing",
+    "A flower does not think of competing with the flower next to it, it just blossoms."
 ];
 var quoteDiv = document.getElementById("quoteDiv");
 var randomNr = Math.floor(Math.random()* quoteArray.length);
@@ -16,6 +16,4 @@ var randomNr = Math.floor(Math.random()* quoteArray.length);
 function renderQuote() {
     quoteDiv.innerHTML = `<i><q>${quoteArray[randomNr]}</q></i>`;
 }
-
-
 renderQuote();
