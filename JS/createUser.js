3d05b2bf638ca8b5f12c7478d23345ec30a8924e
anewardeberg <anelan@hotmail.no>
2020-05-19 12:13:44 +0200

    started on newMember creates div

--- JS/createUser.js
+++ JS/createUser.js
@@ -27,6 +27,7 @@ function createNewUser(event) {
       
       
     console.log("this is")
+    // Passord må inneholde (?)
     var passw =   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
     if(password.match(passw)) { 
       alert("Correct, try another...");
@@ -40,7 +41,7 @@ function createNewUser(event) {
         //Pushes the new users into the array so it doesnt erase the previous one. 
         users.push(user);
         window.localStorage.setItem("users", JSON.stringify(users));
-       // renderUserList();
+        renderUserList();
         addUser();
         // add users into the option field of assignmember. 
         // resets the input field so the values is default.
@@ -58,35 +59,40 @@ function createNewUser(event) {
       users = [];
   }
   
-  /*
+
   // the function that gets the users on the page. 
   function renderUserList() {
     // Link to a div in the html file.
-    let usersEl = document.getElementById("outputDiv");
+    let usersEl = document.getElementById("circleOverview");
     // get users from local storage or if its null an empty array.
-    
+    let users = JSON.parse(window.localStorage.getItem("users")) || [];
     usersEl.innerHTML ="";
     // usersEl.innerHTML="<span>Show group members</span>";
     for(const user of users){
+      //generates random color
+      generateRandomColor();
       // creates a new div element
       const userEl = document.createElement("div");
-      const {username,password} = user;
+      const {username} = user;
       // sets the innerhtml to be the username and password.
       userEl.innerHTML = `
-      <div class="objectDiv">
-        <h4>User</h4>
-        <div>
-          <strong>Username:</strong> ${username}
-          <br>
-          <strong>Password:</strong> ${password}
-        </div>
-        <br>
-        <br>
-        <br>
-        <br>
-      </div>`;
+        <div class="circleDiv"
+          style="
+            border-width:2px;
+            border-style:solid;
+            border-color:${randomColor};
+          ">
+              <div class="headerCircles">
+              </div>
+              <div class="circleTxt">${username}</div>
+        </div>`;
       usersEl.appendChild(userEl);
     }
-    teamMembers = [];
-*/
-  
+    users = [];
+  }
+
+  function generateRandomColor() {
+    var randomColor =  "#"+((1<<24)*Math.random()|0).toString(16);
+    return randomColor;
+
+  }
