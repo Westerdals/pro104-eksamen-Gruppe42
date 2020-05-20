adafe59b5cf57a607553e8f154ded8e2195dec67
Benjamin <vedlogb@gmail.com>
2020-05-20 15:10:41 +0200

    starting to setup login page

--- JS/createUser.js
+++ JS/createUser.js
@@ -6,7 +6,7 @@ function createNewUser(event) {
   
     //gets the name and password value from the input field
     const username = document.querySelector("[name = 'username']").value;
-    const password = document.querySelector("[name = 'password']").value;
+    const password = document.querySelector("[name = 'psw']").value;
     const password_2 = document.querySelector("[name = 'password_2']").value;
    
     // creates a teamMember from the information.
@@ -25,23 +25,22 @@ function createNewUser(event) {
         alert("passwords does not match")
     }else{
       
-      
+      /*
     console.log("this is")
     // Passord må inneholde (?)
-    var passw =   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    var passw =   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
     if(password.match(passw)) { 
-      alert("Correct, try another...");
+      console.log("congratz");
     }
     else { 
-        alert('Wrong...!')
+        alert('Wrong...!');
         return;
     }
-
+*/
 
         //Pushes the new users into the array so it doesnt erase the previous one. 
         users.push(user);
         window.localStorage.setItem("users", JSON.stringify(users));
-        renderUserList();
         addUser();
         // add users into the option field of assignmember. 
         // resets the input field so the values is default.
@@ -59,40 +58,3 @@ function createNewUser(event) {
       users = [];
   }
   
-
-  // the function that gets the users on the page. 
-  function renderUserList() {
-    // Link to a div in the html file.
-    let usersEl = document.getElementById("circleOverview");
-    // get users from local storage or if its null an empty array.
-    let users = JSON.parse(window.localStorage.getItem("users")) || [];
-    usersEl.innerHTML ="";
-    // usersEl.innerHTML="<span>Show group members</span>";
-    for(const user of users){
-      //generates random color
-      generateRandomColor();
-      // creates a new div element
-      const userEl = document.createElement("div");
-      const {username} = user;
-      // sets the innerhtml to be the username and password.
-      userEl.innerHTML = `
-        <div class="circleDiv"
-          style="
-            border-width:2px;
-            border-style:solid;
-            border-color:${randomColor};
-          ">
-              <div class="headerCircles">
-              </div>
-              <div class="circleTxt">${username}</div>
-        </div>`;
-      usersEl.appendChild(userEl);
-    }
-    users = [];
-  }
-
-  function generateRandomColor() {
-    var randomColor =  "#"+((1<<24)*Math.random()|0).toString(16);
-    return randomColor;
-
-  }
--- JS/main.js
+++ JS/main.js
@@ -17,3 +17,43 @@ function renderQuote() {
     quoteDiv.innerHTML = `<i><q>${quoteArray[randomNr]}</q></i>`;
 }
 renderQuote();
+
+
+
+
+  // the function that gets the users on the page. 
+  function renderUserList() {
+    // Link to a div in the html file.
+    let usersEl = document.getElementById("circleOverview");
+    // get users from local storage or if its null an empty array.
+    let users = JSON.parse(window.localStorage.getItem("users")) || [];
+    usersEl.innerHTML ="";
+    // usersEl.innerHTML="<span>Show group members</span>";
+    for(const user of users){
+      //generates random color
+      generateRandomColor();
+      // creates a new div element
+      const userEl = document.createElement("div");
+      const {username} = user;
+      // sets the innerhtml to be the username and password.
+      userEl.innerHTML = `
+        <div class="circleDiv"
+          style="
+            border-width:2px;
+            border-style:solid;
+            border-color:${randomColor};
+          ">
+              <div class="headerCircles">
+              </div>
+              <div class="circleTxt">${username}</div>
+        </div>`;
+      usersEl.appendChild(userEl);
+    }
+    users = [];
+  }
+
+  function generateRandomColor() {
+    var randomColor =  "#"+((1<<24)*Math.random()|0).toString(16);
+    return randomColor;
+
+  }
